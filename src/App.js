d78f3aebe97d3ef9c4f9273f989c5ed6ee0e6544
imranwpsi <imran.wpsi@gmail.com>
2022-09-19 21:06:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/home';
+import Create from './pages/create';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to={"/"} className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/create"} className="nav-link">Create</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create' element={<Create />} />
+        </Routes>
+      </div>
     </div>
   );
 }
